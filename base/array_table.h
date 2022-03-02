5185aa68a808992b699cf6eb73205f3039c0060e
ParkerProbe <kolessnikoff.d.e@gmail.com>
2022-03-02 18:18:32 +0300

    Denis::Add array_table.h fix

--- base/array_table.h
+++ base/array_table.h
@@ -6,21 +6,23 @@
 
 class ArrayTable : public Table
 {
-  TableString* data_;
-  int size_;
-  int capacity_;
-  void Repack();
+    TableString* data_;
+    int size_;
+    int capacity_;
+    void Repack();
 public:
-  ArrayTable() : BaseTable()
-  {
-    size = 0;
-    capacity_ = 100;
-    data = new TableString[capacity_];
-  }
-  void Delete(const string& key);
-  void Insert(const TableString& data);
-  TableString* Find(const string& key);
-  void Print();
-  ~ArrayTable();
+    ArrayTable() : BaseTable()
+    {
+        size = 0;
+        capacity_ = 100;
+        data = new TableString[capacity_];
+    }
+    void Delete(const string& key);
+    void Insert(const TableString& data);
+    TableString* Find(const string& key);
+    void Print();
+    ~ArrayTable();
 };
+
+
 #endif
