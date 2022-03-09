ed837c9e6394b97c13e9a20e4e06de547ec7288b
ParkerProbe <kolessnikoff.d.e@gmail.com>
2022-03-10 01:04:05 +0300

    Denis::add tests, fix padding (2)

--- base/table.h
+++ base/table.h
@@ -1,3 +1,4 @@
+ 
 #ifndef _TABLE_H_
 #define _TABLE_H_
 
@@ -7,15 +8,15 @@
 //It is possible that new methods or fields will be added
 class Table
 {
-	virtual TableString* find_str(const std::string& key) = 0;
+  virtual TableString* find_str(const std::string& key) = 0;
 public:
-	virtual TableBody* find(const std::string& key) = 0;
-	virtual void insert(const TableString& data) = 0;
-	virtual void erase(const std::string& key) = 0;
+  virtual TableBody* find(const std::string& key) = 0;
+  virtual void insert(const TableString& data) = 0;
+  virtual void erase(const std::string& key) = 0;
   virtual void print() = 0;
   virtual ~Table(){}
-	virtual bool empty();
-	virtual bool full();
+  virtual bool empty();
+  virtual bool full();
 };
   
 #endif // _TABLE_H_
