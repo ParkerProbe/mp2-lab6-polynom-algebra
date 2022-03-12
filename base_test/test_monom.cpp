686fcd9709f62202885dd7d8ebae7635568a7da6
a1xsa <tsvetkov-alexey@mail.ru>
2022-03-12 22:14:53 +0300

    tab

--- base_test/test_monom.cpp
+++ base_test/test_monom.cpp
@@ -4,61 +4,61 @@
 
 TEST(TMonom, can_create_monom_default)
 {
-    ASSERT_NO_THROW(Monom m());
+  ASSERT_NO_THROW(Monom m());
 }
 TEST(TMonom, can_create_monom)
 {
-    ASSERT_NO_THROW(Monom m(2.5, 123));
+  ASSERT_NO_THROW(Monom m(2.5, 123));
 }
 TEST(TMonom, can_create_monom_copy)
 {
-    ASSERT_NO_THROW(Monom m());
+  ASSERT_NO_THROW(Monom m());
 }
 TEST(TMonom, cant_create_monom_with_degree_more_than_max_degree)
 {
-    ASSERT_ANY_THROW();
+  ASSERT_ANY_THROW();
 }
 TEST(TMonom, cant_create_monom_with_degree_less_than_null)
 {
-    ASSERT_ANY_THROW(Monom m(2.5, -123));
+  ASSERT_ANY_THROW(Monom m(2.5, -123));
 }
 
 TEST(TMonom, true_if_first_monom_is_less_than_second_monom)
 {
-    Monom m();
-    Monom m1();
-    EXPECT_EQ(true, m < m1);
+  Monom m();
+  Monom m1();
+  EXPECT_EQ(true, m < m1);
 }
 
 TEST(TMonom, can_plus_two_monoms)
 {
-    Monom m();
-    Monom m1();
-    ASSERT_NO_THROW(m + m1);
+  Monom m();
+  Monom m1();
+  ASSERT_NO_THROW(m + m1);
 }
 
 TEST(TMonom, can_minus_two_monoms)
 {
-    Monom m();
-    Monom m1();
-    ASSERT_NO_THROW(m - m1);
+  Monom m();
+  Monom m1();
+  ASSERT_NO_THROW(m - m1);
 }
 
 TEST(TMonom, can_multiply_two_monoms)
 {
-    Monom m();
-    Monom m1();
-    ASSERT_NO_THROW(m * m1);
+  Monom m();
+  Monom m1();
+  ASSERT_NO_THROW(m * m1);
 }
 
 TEST(TMonom, can_take_derivative_to_param)
 {
-    Monom m();
-    ASSERT_NO_THROW(m.derivative('x'));
+  Monom m();
+  ASSERT_NO_THROW(m.derivative('x'));
 }
 
 TEST(TMonom, can_take_integral_to_param)
 {
-    Monom m();
-    ASSERT_NO_THROW(m.integral('x'));
+  Monom m();
+  ASSERT_NO_THROW(m.integral('x'));
 }
--- base_test/test_polinom.cpp
+++ base_test/test_polinom.cpp
@@ -4,83 +4,83 @@
 
 TEST(TPolynom, can_create_polynom_default)
 {
-    ASSERT_NO_THROW(Polynom pl());
+  ASSERT_NO_THROW(Polynom pl());
 }
 
 TEST(TPolynom, can_create_polynom_copy)
 {
-    ASSERT_NO_THROW(Polynom pl());
+  ASSERT_NO_THROW(Polynom pl());
 }
 
 TEST(TPolynom, can_create_polynom_from_string)
 {
-    ASSERT_NO_THROW(Polynom pl());
+  ASSERT_NO_THROW(Polynom pl());
 }
 
 TEST(TPolynom, can_create_polynom_from_list)
 {
-    ASSERT_NO_THROW(Polynom pl());
+  ASSERT_NO_THROW(Polynom pl());
 }
 
 TEST(TPolynom, can_add_monom_to_polynom)
 {
-    Polynom pl();
-    ASSERT_NO_THROW(pl.add_monom());
+  Polynom pl();
+  ASSERT_NO_THROW(pl.add_monom());
 }
 
 TEST(TPolynom, can_sort_monoms_of_polynom)
 {
-    Polynom pl();
-    ASSERT_NO_THROW(pl.sort());
+  Polynom pl();
+  ASSERT_NO_THROW(pl.sort());
 }
 
 TEST(TPolynom, can_plus_two_polynoms)
 {
-    Polynom pl1();
-    Polynom pl2();
-    ASSERT_NO_THROW(pl1 + pl2);
+  Polynom pl1();
+  Polynom pl2();
+  ASSERT_NO_THROW(pl1 + pl2);
 }
 
 TEST(TPolynom, can_minus_two_polynoms)
 {
-    Polynom pl1();
-    Polynom pl2();
-    ASSERT_NO_THROW(pl1 - pl2);
+  Polynom pl1();
+  Polynom pl2();
+  ASSERT_NO_THROW(pl1 - pl2);
 }
 
 TEST(TPolynom, can_multiply_two_polynoms)
 {
-    Polynom pl1();
-    Polynom pl2();
-    ASSERT_NO_THROW(pl1 * pl2);
+  Polynom pl1();
+  Polynom pl2();
+  ASSERT_NO_THROW(pl1 * pl2);
 }
 
 TEST(TPolynom, can_calculate_value_in_point)
 {
-    Polynom pl();
-    ASSERT_NO_THROW(pl.calculate_in_point());
+  Polynom pl();
+  ASSERT_NO_THROW(pl.calculate_in_point());
 }
 
 TEST(TPolynom, can_take_derivative)
 {
-    Polynom pl;
-    ASSERT_NO_THROW(pl.derivative());
+  Polynom pl;
+  ASSERT_NO_THROW(pl.derivative());
 }
 
 TEST(TPolynom, can_take_integral)
 {
-    Polynom pl;
-    ASSERT_NO_THROW(pl.integral());
+  Polynom pl;
+  ASSERT_NO_THROW(pl.integral());
 }
 
 TEST(TPolynom, can_write_to_file)
 {
-    Polynom pl;
-    ASSERT_NO_THROW(pl.write_to_file());
+  Polynom pl;
+  ASSERT_NO_THROW(pl.write_to_file());
 }
 
 TEST(TPolynom, can_read_from_file)
 {
-    Polynom pl;
-    ASSERT_NO_THROW(pl.read_from_file());
+  Polynom pl;
+  ASSERT_NO_THROW(pl.read_from_file());
 }
