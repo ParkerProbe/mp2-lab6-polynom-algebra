0f9b81462d4178cb5dfcac4f106052ec20ebcd5f
ParkerProbe <kolessnikoff.d.e@gmail.com>
2022-03-19 19:22:07 +0300

    Denis::fix padding_3

--- base/table.h
+++ base/table.h
@@ -10,54 +10,53 @@
 class Table
 {
 protected:
-	int data_cnt;
-	virtual TableString* find_str(const std::string& key) = 0;
+    int data_cnt;
+    virtual TableString* find_str(const std::string& key) = 0;
 public:
-  Table() : data_cnt(0) {}
-	virtual ~Table() {}
+    Table() : data_cnt(0) {}
+    virtual ~Table() {}
 
-	virtual TableBody* find(const std::string& key) = 0;
-	virtual void insert(const TableString& data) = 0;
-	virtual void erase(const std::string& key) = 0;
-	virtual void print() const = 0;
-	virtual bool is_full() const = 0;
+    virtual TableBody* find(const std::string& key) = 0;
+    virtual void insert(const TableString& data) = 0;
+    virtual void erase(const std::string& key) = 0;
+    virtual void print() const = 0;
+    virtual bool is_full() const = 0;
 
-	// Set to first record
-	virtual bool reset() = 0;
+    // Set to first record
+    virtual bool reset() = 0;
 
-	virtual bool is_tab_ended() const = 0;
+    virtual bool is_tab_ended() const = 0;
 
-	// Next record
-	// return false for last record
-	virtual bool go_next() = 0;
+    // Next record
+    // return false for last record
+    virtual bool go_next() = 0;
 
-	// Get value of current iterable record
-	virtual TableString* get_value() = 0;
+    // Get value of current iterable record
+    virtual TableString* get_value() = 0;
   
-	virtual TableIterator begin() const = 0;
-	virtual TableIterator end() const = 0;
+    virtual TableIterator begin() const = 0;
+    virtual TableIterator end() const = 0;
 
 
 
-	virtual bool empty() const
-	{
-		return data_cnt == 0;
-	}
-	virtual int get_size() const
-	{
-		return data_cnt;
-	}
+    virtual bool empty() const
+    {
+        return data_cnt == 0;
+    }
+    virtual int get_size() const
+    {
+        return data_cnt;
+    }
 
-	friend std::ostream& operator<<(std::ostream& os, const Table& tab)
-	{
-	    std::cout << "Table printing" << std::endl;
-	    for (TableIterator it = tab.begin(); it != tab.end(); ++it)
-  	    {
+    friend std::ostream& operator<<(std::ostream& os, const Table& tab)
+    {
+        std::cout << "Table printing" << std::endl;
+        for (TableIterator it = tab.begin(); it != tab.end(); ++it) {
             os << " Key" << (*it).key << " Val "
                 << (*it).body.poly_string << std::endl;
-  	    }
-		return os;
-	}
+        }
+        return os;
+    }
 
 
 };
